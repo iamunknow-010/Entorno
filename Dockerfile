c91e6b7bfaba8674afa0ad2ffbdf9772469e3f3b
iamunknow-010 <xxkatto01xx@gmail.com>
2026-03-21 21:07:37 -0300

    Agregando script app.py para mantener el entorno activo

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # 5. Comando por defecto al iniciar el contenedor
-CMD ["python", "--version"]
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,8 @@
+import time
+
+print("🚀 Entorno de aprendizaje iniciado con éxito.")
+print("Conexión con la base de datos configurada (simulada).")
+
+# Este bucle evita que el contenedor se apague
+while True:
+    time.sleep(3600)  # Duerme una hora y repite
